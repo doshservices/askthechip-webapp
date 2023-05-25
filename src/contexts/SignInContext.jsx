d652ef7abf986feaad3c75eb7c94ee197c560461
Oladimeji Abdrahman Adebayo <abdrahmanoladimeji02@gmail.com>
2023-05-25 12:18:10 +0100

    update: implement the fixes pointed out

--- src/contexts/SignInContext.jsx
+++ src/contexts/SignInContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useState } from "react";
 import {SignInModal } from "./../components";
 
-export const SigninContext = createContext();
+export const SignInContext = createContext();
 
 const SignInProvider = ({ children }) => {
     const [signInModal, setSignInModal] = useState(false);
@@ -14,14 +14,14 @@ const SignInProvider = ({ children }) => {
     }
     const value = { openSignInModal, closeSignInModal }
     return (
-        <SigninContext.Provider value={value}>
+        <SignInContext.Provider value={value}>
             <div className="relative">
             {children}
             <div className="absolute top-0 right-0 w-full flex justify-center mx-auto">
                {signInModal && <SignInModal />}
             </div>
             </div>
-        </SigninContext.Provider>
+        </SignInContext.Provider>
     )
 }
 
