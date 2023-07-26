98e8e38a9b47b133c0ce9aec953a79bf68867658
Oladimeji Abdrahman Adebayo <abdrahmanoladimeji02@gmail.com>
2023-07-26 16:50:20 +0100

    fix: fixed bg color

--- src/components/MobileLayout.jsx
+++ src/components/MobileLayout.jsx
@@ -1,8 +1,8 @@
 import MobileNav from "./MobileNav";
-
 const MobileLayout = ({ children }) => {
+  const pathname = window.location.pathname;
   return (
-    <div className="flex flex-col max-h-screen overflow-y-hidden sm:hidden">
+    <div className={pathname === '/messages'? 'flex flex-col max-h-screen overflow-y-hidden sm:hidden': 'flex flex-col max-h-screen overflow-y-hidden sm:hidden bg-light'}>
       <div className="h-[calc(100vh_-_4.5rem)] overflow-y-auto">{children}</div>
       <div className="h-[4.5rem]">
         <MobileNav />
