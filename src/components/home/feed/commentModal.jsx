1f28a6df72c97ed124d37f2751e669a30e798811
prosper edafe <mamusedafe19@gmail.com>
2023-12-06 09:38:43 +0100

    comment modal completed

--- src/components/home/feed/commentModal.jsx
+++ src/components/home/feed/commentModal.jsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from "../../../contexts/AuthContext/AuthContext";
-import { useEffect } from "react";
 import axios from "axios";
 
 export const CommentModal = ({ close, post }) => {
-    console.log(post);
+    // console.log(post);
 
     const { token } = useAuth();
     const [comment, setComment] = useState([])
-    console.log(comment);
+    // const [optionsModal, setOptionsModal] = useState(false)
+
+    // console.log(comment);
     const getComments = async () => {
         try {
             const response = await axios.get(
@@ -27,14 +28,13 @@ export const CommentModal = ({ close, post }) => {
         }
     };
 
-
     useEffect(() => {
         getComments()
     }, [])
 
     return (
         <section className="comment__modal">
-            <svg onClick={close} className="close__btn" width="40" height="40" fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <svg onClick={close} className="close__btn" width="35" height="35 " fill="#fff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 2.25c-5.376 0-9.75 4.374-9.75 9.75s4.374 9.75 9.75 9.75 9.75-4.374 9.75-9.75S17.376 2.25 12 2.25Zm3.53 12.22a.75.75 0 1 1-1.06 1.06L12 13.06l-2.47 2.47a.75.75 0 0 1-1.06-1.06L10.94 12 8.47 9.53a.75.75 0 0 1 1.06-1.06L12 10.94l2.47-2.47a.75.75 0 0 1 1.06 1.06L13.06 12l2.47 2.47Z"></path>
             </svg>
             <div style={{ maxWidth: post?.postImg ? "800px " : "450px" }} className="comment__modal__wrapper">
@@ -54,6 +54,14 @@ export const CommentModal = ({ close, post }) => {
                                     <p className="fullname">{post?.userId?.fullName}</p>
                                     <p className="content">{comments?.text}</p>
                                 </div>
+                                <svg className="block ml-auto cursor-pointer mt-2" width="18" height="18" fill="#2d2d2d" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M12 14.25a2.25 2.25 0 1 0 0-4.5 2.25 2.25 0 0 0 0 4.5Z"></path>
+                                    <path d="M12 21.75a2.25 2.25 0 1 0 0-4.5 2.25 2.25 0 0 0 0 4.5Z"></path>
+                                    <path d="M12 6.75a2.25 2.25 0 1 0 0-4.5 2.25 2.25 0 0 0 0 4.5Z"></path>
+                                </svg>
+                                {/* <div className="options">
+                                    <button>Delete Comment</button>
+                                </div> */}
                             </div>
                         )
                     })}
