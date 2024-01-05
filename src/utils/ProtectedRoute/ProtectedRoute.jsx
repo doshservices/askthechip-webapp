e5564fd109a2e85bdd2a29f558a33479b63b728a
prosper edafe <mamusedafe19@gmail.com>
2024-01-05 20:32:40 +0100

    fixed

--- src/utils/ProtectedRoute/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute/ProtectedRoute.jsx
@@ -9,13 +9,40 @@ import { clearJwt } from "../../store/slice/authSlice";
 import { clearChatUserId } from "../../store/slice/chatViewSlice";
 import { clearUser } from "../../store/slice/userSlice";
 import { clearId } from "../../store/slice/notificationSlice";
+import { inform } from "../../App";
 
 const ProtectedRoute = () => {
+  const dispatch = useDispatch()
   const { user, setUser } = useAuth();
   const [isLoading, setIsLoading] = useState(true);
-  const token = useSelector((state) => state?.jwtSlice?.jwt)
-  const decoded = jwtDecode(token)
-  const dispatch = useDispatch()
+  const token = useSelector((state) => state?.jwtSlice?.jwt);
+  const currentTimestamp = Math.floor(Date.now() / 1000);
+
+  const clearAuthUser = () => {
+    localStorage.removeItem("authUser")
+    localStorage.removeItem("token")
+    localStorage.removeItem("ask-un-id")
+    localStorage.removeItem("ask-u-mail")
+    setUser(null);
+    inform("Session time out. Please login");
+    dispatch(clearJwt())
+    dispatch(clearChatUserId())
+    dispatch(clearUser())
+    dispatch(clearId())
+  }
+
+  useEffect(() => {
+    if (!token) {
+      clearAuthUser()
+    }
+  }, [])
+
+  if (token) {
+    const decoded = jwtDecode(token)
+    if (decoded?.exp && decoded?.exp < currentTimestamp) {
+      clearAuthUser()
+    }
+  }
 
   useEffect(() => {
     const userData = localStorage.getItem('authUser');
@@ -28,24 +55,6 @@ const ProtectedRoute = () => {
     }
   }, []);
 
-  const currentTimestamp = Math.floor(Date.now() / 1000);
-
-  const clearAuthUser = () => {
-    localStorage.removeItem("authUser")
-    localStorage.removeItem("token")
-    localStorage.removeItem("ask-un-id")
-    localStorage.removeItem("ask-u-mail")
-    setUser(null);
-    dispatch(clearJwt())
-    dispatch(clearChatUserId())
-    dispatch(clearUser())
-    dispatch(clearId())
-  }
-
-  if (decoded?.exp && decoded?.exp < currentTimestamp) {
-    clearAuthUser()
-  }
-
   if (isLoading) {
     return (
       <div className="bg-gray-200 h-full md:h-[calc(100vh_-_60px)] overflow-y-auto">
@@ -56,6 +65,6 @@ const ProtectedRoute = () => {
     )
   }
 
-  return user ? <Outlet /> : <Navigate to="/login" />
+  return user && token ? <Outlet /> : <Navigate to="/login" />
 }
 export default ProtectedRoute;
